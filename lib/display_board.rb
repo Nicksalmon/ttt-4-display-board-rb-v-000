0dab807f1380b8edb65cb75b5c28d45962c1d6f0
Nick Salmon <nickmidspace@gmail.com>
2020-02-18 05:46:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 def display_board(board)
-  puts "[" "," "," "," "," "," "," "," "," "]"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 
 
 
